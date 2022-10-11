694b5e96ec396dd6821f5c9551bf46bb8815b9ff
PardeshDhakal <pardesh.dhakal1@gmail.com>
2022-10-11 01:41:53 -0400

    Home Screen with Dashboard button

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -2,12 +2,45 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, FlatList } from 'react-native';
+//Nav Bar Imports
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons'
+// Screens
+import HomeScreen from './HomeScreen';
+import RecipeScreen from './RecipeScreen';
+import ProfileScreen from './ProfileScreen';
 
+const homeName = 'Home';
+const recipeName = 'Recipe';
+const profileName = 'Profile';
+
+const Tab = createBottomTabNavigator();
 
 export default function MainContainer(){
     return(
-        <View>
-            <Text> Hi </Text>
-        </View> 
+        <NavigationContainer> 
+            <Tab.Navigator
+            initialRouteName={homeName} screenOptions= {({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+                    let rn = route.name;
+
+                    if(rn == homeName){
+                        iconName = focused ? 'home': 'home-outline';
+                    } else if (rn == recipeName){
+                        iconName = focused ? 'list':'list-outline';
+                    } else if (rn == profileName){
+                        iconName = focused ? 'settings':'settings-outline';
+                    }
+                    return <Ionicons name = {iconName} size={size} color={color}/>
+                }
+            })}> 
+
+            <Tab.Screen name={homeName} component = {HomeScreen}/>
+            
+
+        </Tab.Navigator>
+        </NavigationContainer>
     )
 }
